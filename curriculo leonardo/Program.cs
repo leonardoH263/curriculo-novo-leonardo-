d60973c4355fc0de4464fc5272ac9a6a8e4e043c
leonardo <leonardo.pereira263@etec.sp.gov.br>
2022-11-10 22:02:20 -0300

    gravação inicial

---
+++ curriculo leonardo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curriculo_leonardo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

f3fe69c76f3ae21326ceeae9494dc9dd6fafdbdb
Leonardo Henrique <112669885+leonardoH263@users.noreply.github.com>
2022-11-10 22:07:43 -0300

    Update Program.cs

--- curriculo leonardo/Program.cs
+++ curriculo leonardo/Program.cs
@@ -9,7 +9,54 @@ namespace curriculo_leonardo
     class Program
     {
         static void Main(string[] args)
-        {
+        { string nome;
+            int idade;
+            int telefone;
+            int rg;
+            int nascimento;
+            string genero;
+            string endereço;
+            string experiencia;
+            string formação;
+            string carta;
+
+
+
+
+            Console.WriteLine("digite seu nome");
+        nome: Console.ReadLine();
+
+            Console.WriteLine("digite sua idade");
+        idade: Console.ReadLine();
+
+            Console.WriteLine("digite seu telefone");
+        telefone: Console.ReadLine();
+
+            Console.WriteLine("digite seu rg");
+        rg: Console.ReadLine();
+
+            Console.WriteLine("digite seu nascimento");
+        nascimento: Console.ReadLine();
+
+            Console.WriteLine("digite seu genero");
+        genero: Console.ReadLine();
+
+            Console.WriteLine("digite seu endereço");
+        endereço: Console.ReadLine();
+
+            Console.WriteLine("digite sua experiencia");
+        experiencia: Console.ReadLine();
+
+            Console.WriteLine("digite sua formação");
+        formação: Console.ReadLine();
+
+            Console.WriteLine("digite sua carta");
+        carta: Console.ReadLine();
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
